f9d16c2596d2af11e966063ef61276b31603a7aa
vviiaannaa <viana.alipanahifard@yahoo.com>
2025-02-14 12:00:56 +0330

    bucketedit

--- home/views.py
+++ home/views.py
@@ -1,8 +1,11 @@
-from django.shortcuts import render,get_object_or_404
+from django.shortcuts import render,get_object_or_404,redirect
 from django.views import View
 from .models import Product
 from . import tasks
 from bucket import Bucket
+from django.contrib import messages
+from utils import IsAdminUserMixin
+from orders.forms import CartAddForm
 
 
 class HomeView(View):
@@ -21,3 +24,16 @@ class BucketView(View):
         objects=tasks.all_bucket_objects_task()
         print(objects)
         return render(request,self.template_name,{'objects':objects})
+
+class DeleteBucketObject(IsAdminUserMixin, View):
+	def get(self, request, key):
+		tasks.delete_object_task.delay(key)
+		messages.success(request, 'your object will be delete soon.', 'info')
+		return redirect('home:bucket')
+
+
+class DownloadBucketObject(IsAdminUserMixin, View):
+	def get(self, request, key):
+		tasks.download_object_task.delay(key)
+		messages.success(request, 'your download will start soon.', 'info')
+		return redirect('home:bucket')
